842fc3ef5e69a1a8e0d0acc5a5a306d2a4731117
Prince Codes <princecodes247@gmail.com>
2022-06-13 01:01:33 -0400

    Add Footer and Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>
+            <strong>React</strong> by <a href="">SDSF</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Header;
