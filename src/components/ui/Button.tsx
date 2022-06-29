796c9e057443f758c4d699e7d57cceee077db685
Eric Afes <eriicafes@gmail.com>
2022-06-29 17:11:11 +0100

    Make Button component forward ref

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React from "react";
+import React, { forwardRef } from "react";
 import { Spinner } from "../icons/Spinner";
 
 type Props = {
@@ -12,21 +12,24 @@ type Props = {
 
 type Variant = "primary" | "auth";
 
-export const Button: React.FC<Props> = ({
-  children,
-  variant,
-  override,
-  outline,
-  loading,
-  className,
-  ...props
-}) => {
-  return (
-    <button className={styles(variant, override, outline, className)} {...props}>
-      {loading ? <Spinner className="w-6 h-6" /> : children}
-    </button>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, Props>(
+  (
+    { children, variant, override, outline, loading, className, ...props },
+    ref
+  ) => {
+    return (
+      <button
+        ref={ref}
+        className={styles(variant, override, outline, className)}
+        {...props}
+      >
+        {loading ? <Spinner className="w-6 h-6" /> : children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 const styles = (
   variant: Variant = "primary",
@@ -39,13 +42,13 @@ const styles = (
     variant === "auth" && [
       outline
         ? [
-          "bg-transparent text-black dark:text-white border border-zinc-300 dark:border-zinc-600",
-          "hover:border-zinc-900 dark:hover:border-zinc-300",
-        ]
+            "bg-transparent text-black dark:text-white border border-zinc-300 dark:border-zinc-600",
+            "hover:border-zinc-900 dark:hover:border-zinc-300",
+          ]
         : [
-          "bg-zinc-900 dark:bg-zinc-100 text-white dark:text-black border border-zinc-900 dark:border-zinc-300",
-          "hover:bg-transparent dark:hover:bg-transparent hover:text-black dark:hover:text-white",
-        ],
+            "bg-zinc-900 dark:bg-zinc-100 text-white dark:text-black border border-zinc-900 dark:border-zinc-300",
+            "hover:bg-transparent dark:hover:bg-transparent hover:text-black dark:hover:text-white",
+          ],
     ],
     className,
   ]);
